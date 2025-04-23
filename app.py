434c9516f55b96a0288a849487c32bdf594c2e9d
Bilal <86686062+Bilal-A-G@users.noreply.github.com>
2025-04-22 21:55:54 -0400

    Using RAKE now for keyword extraction, need to figure out how to do similarity, can't use ML due to reliability issues seemingly

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template, request, send_file
-import nltk as nlp
-import re
 import json
 from jinja2 import FileSystemLoader, Environment
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
+from rake_nltk import Rake
 from weasyprint import HTML, CSS
 from io import BytesIO
 
@@ -22,9 +19,14 @@ def profile():
 def createEditProfile():
     return render_template("submitted.html")
 
+def getKeywords(originalText):
+    rake = Rake()
+
+    rake.extract_keywords_from_text(originalText)
+    return rake.get_ranked_phrases_with_scores()
+
 @app.route("/submit", methods = ["POST", "GET"])
 def submitted():
-    punctuation = ".,!&'?;:/()-[]@" + '"'
     jobDesc = request.form["jobDesc"]
     profile = request.form["profile"]
     jsonData = json.loads(profile)
@@ -32,46 +34,25 @@ def submitted():
     jinjaEnv = Environment(loader=FileSystemLoader("./resumeTemplates"))
     template = jinjaEnv.get_template("template.html")
 
-    fileInfo = template.render(name=jsonData["name"], 
-                               education=jsonData["education"], 
-                               email=jsonData["email"], 
-                               website=jsonData["website"], 
-                               workExperience=jsonData["workExperience"], 
-                               projects=jsonData["projects"],
+    jobDescKeywords = getKeywords(jobDesc)
+    print(jobDescKeywords)
+
+    projectsCopy = jsonData["projects"]
+    projectDescriptions = jsonData["projects"][0]["projectDescriptions"]
+    relevantDescriptions = []
+    for description in projectDescriptions:
+        descriptionKeywords = getKeywords(description)
+        print(descriptionKeywords)
+
+    fileInfo = template.render(name=jsonData["name"],
+                               education=jsonData["education"],
+                               email=jsonData["email"],
+                               website=jsonData["website"],
+                               workExperience=jsonData["workExperience"],
+                               projects=projectsCopy,
                                skills=jsonData["skills"], accomplishments=jsonData["accomplishments"])
-    print(fileInfo)
     pdfFile = BytesIO()
     HTML(string=fileInfo).write_pdf(pdfFile, stylesheets=[CSS("./resumeTemplates/template.css")])
     pdfFile.seek(0)
 
     return send_file(pdfFile, mimetype="application/pdf" ,as_attachment=True, download_name="Resume.pdf")
-
-    #Remove all punctuation
-    for char in punctuation:
-        jobDesc = jobDesc.replace(char, " ")
-
-    #Convert everything to lowercase
-    jobDesc = jobDesc.lower()
-
-    #Replace all non ascii characters with a space
-    jobDesc = re.sub(r'[^\x00-\x7f]', r' ', jobDesc)
-
-    #Break the text into individual words
-    tokens = nlp.word_tokenize(jobDesc)
-
-    #Remove all stop words, ie things like the, I, me, my, etc.
-    allStopWords = set(stopwords.words("english"))
-    filtered = []
-
-    for token in tokens:
-        if token not in allStopWords:
-            filtered.append(token)
-
-    #Lematize word, reduce all words to their root dictionary form, ie running -> run
-    lemmatized = []
-    lemmatizer = WordNetLemmatizer()
-    for token in filtered:
-        lemmatized.append(lemmatizer.lemmatize(token))
-
-    print(lemmatized)
-    return lemmatized
