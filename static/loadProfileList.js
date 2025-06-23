83fe855ed039a13af8cb31ec2f3df3dbe037ed8e
Bilal <86686062+Bilal-A-G@users.noreply.github.com>
2025-06-23 09:14:48 -0400

    Holder UI styling
    - Made it so the website now displays the no profiles found message when there are no profiles
    - It also moves the add and import profile buttons further down to not interfere with the list

--- static/loadProfileList.js
+++ static/loadProfileList.js
@@ -1,3 +1,21 @@
+function RecalculateNoprofiles(){
+    const profilesFoundDiv = document.getElementById("profilesFound")
+    const noProfilesDiv = document.getElementById("noProfiles")
+
+    if(localStorage.length > 0){
+        noProfilesDiv.classList.remove("visible")
+        noProfilesDiv.classList.add("hidden")
+        profilesFoundDiv.classList.remove("hidden")
+        profilesFoundDiv.classList.add("visibleFlex")
+    }
+    else{
+        profilesFoundDiv.classList.remove("visibleFlex")
+        profilesFoundDiv.classList.add("hidden")
+        noProfilesDiv.classList.remove("hidden")
+        noProfilesDiv.classList.add("visible")
+    }
+}
+
 function CreateProfileListEntry(parent, index, name){
     const actualName = name.split('|')[0]
     const date = name.split('|')[1]
@@ -34,6 +52,7 @@ function CreateProfileListEntry(parent, index, name){
         e.preventDefault()
         localStorage.removeItem(name)
         parent.removeChild(backgroundRow)
+        RecalculateNoprofiles()
     })
     deleteButton.appendChild(deleteButtonImage)
 
@@ -51,3 +70,5 @@ const numItems = localStorage.length
 for(let i = 0; i < numItems; i++){
     CreateProfileListEntry(document.getElementById("profilesDropdown"), i, localStorage.key(i))
 }
+
+RecalculateNoprofiles()
